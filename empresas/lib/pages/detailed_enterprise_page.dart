a312ad7752f288573c821d2c617699348b56a183
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-08-04 11:34:01 -0300

    Changed to Stateless and added a loadingBuilder to imager

--- empresas/lib/pages/detailed_enterprise_page.dart
+++ empresas/lib/pages/detailed_enterprise_page.dart
@@ -2,30 +2,41 @@ import 'package:empresas/helpers/enterprises_api.dart';
 import 'package:empresas/helpers/entity_classes/enterprise.dart';
 import 'package:flutter/material.dart';
 
-// ignore_for_file: prefer_expression_function_bodies, for better readability on Widgets files
-class DetailedEnterprisePage extends StatefulWidget {
+class DetailedEnterprisePage extends StatelessWidget {
   final Enterprise enterprise;
 
   const DetailedEnterprisePage({Key? key, required this.enterprise}) : super(key: key);
 
   @override
-  _DetailedEnterprisePageState createState() => _DetailedEnterprisePageState();
-}
-
-class _DetailedEnterprisePageState extends State<DetailedEnterprisePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.enterprise.name),
+        title: Text(enterprise.name),
       ),
       body: ListView(
         children: <Widget>[
-          Image.network('${EnterprisesApi.server}${widget.enterprise.photo}'),
+          Image.network(
+            '${EnterprisesApi.server}${enterprise.photo}',
+            loadingBuilder: (context, child, progress) {
+              if (progress == null) {
+                return child;
+              }
+              return SizedBox(
+                height: 200,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    value: progress.expectedTotalBytes != null
+                        ? progress.cumulativeBytesLoaded / progress.expectedTotalBytes!
+                        : null,
+                  ),
+                ),
+              );
+            },
+          ),
           Padding(
             padding: const EdgeInsets.all(12),
             child: Text(
-              widget.enterprise.description,
+              enterprise.description,
               style: Theme.of(context).textTheme.headline5!.copyWith(),
             ),
           )
