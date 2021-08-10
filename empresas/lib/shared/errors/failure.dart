07b82801d977b4d0c2cde4e3a241d6648d437a5c
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-08-10 12:19:39 -0300

    Created the initial declaration of errors and Failures

--- empresas/lib/shared/errors/failure.dart
+++ empresas/lib/shared/errors/failure.dart
@@ -1,3 +1,9 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {}
+
+// General Failures
+class ServerFailure extends Failure {
+  @override
+  List<Object?> get props => List.empty();
+}
