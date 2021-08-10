ab96183cc5d60016cfebfb684effb3087a3ba38c
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-08-10 12:20:16 -0300

    Removed the equatable extension

--- empresas/lib/features/sign_in/domain/repositories/sign_in_repository.dart
+++ empresas/lib/features/sign_in/domain/repositories/sign_in_repository.dart
@@ -1,8 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:empresas/features/sign_in/domain/entities/investor.dart';
 import 'package:empresas/shared/errors/failure.dart';
-import 'package:equatable/equatable.dart';
 
-abstract class SignInRepository extends Equatable {
+abstract class SignInRepository {
   Future<Either<Failure, Investor>> signIn(String email, String password);
 }
