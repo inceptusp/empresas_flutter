1ca94b9f9adaaecb6429732509a8edd00358ecc5
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-08-12 10:16:38 -0300

    Implemented enterprises api with clean arch and tests

--- empresas/lib/shared/data/datasources/enterprises_remote_api.dart
+++ empresas/lib/shared/data/datasources/enterprises_remote_api.dart
@@ -1,47 +1,86 @@
 import 'dart:convert';
 
-import 'package:empresas/features/list_enterprises/domain/entities/enterprise.dart';
+import 'package:empresas/features/list_enterprises/data/datasource/list_enterprises_datasource.dart';
 import 'package:empresas/features/list_enterprises/data/models/enterprise_model.dart';
+import 'package:empresas/features/sign_in/data/datasource/sign_in_datasource.dart';
+import 'package:empresas/features/sign_in/data/models/investor_model.dart';
+import 'package:empresas/shared/errors/exceptions.dart';
 import 'package:http/http.dart' as http;
 
-class EnterprisesApi {
-  static const String server = 'https://empresas.ioasys.com.br/';
-  static const String _apiVersion = 'v1';
+class EnterprisesRemoteApi implements SignInDatasource, ListEnterprisesDatasource {
+  static const String _kServer = 'https://empresas.ioasys.com.br/api/v1';
   static final Map<String, String> _authHeaders = <String, String>{};
+  final http.Client client;
 
-  /// Does a sign in and returns the response JSON as a map. Throws on connection error
-  static Future<Map<String, dynamic>> signIn(String email, String password) async {
+  EnterprisesRemoteApi({required this.client});
+
+  @override
+  Future<List<EnterpriseModel>> getAllEnterprises() async {
+    final response = await client.get(
+      Uri.parse('$_kServer/enterprises'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      }..addAll(_authHeaders),
+    );
+
+    if (response.statusCode == 200) {
+      final List<EnterpriseModel> enterprisesList = List<EnterpriseModel>.empty(growable: true);
+      final Map<String, dynamic> bodyJson = json.decode(response.body);
+      bodyJson['enterprises'].forEach((el) {
+        enterprisesList.add(EnterpriseModel.fromJson(el));
+      });
+
+      return enterprisesList;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<EnterpriseModel>> searchEnterprises(String query) async {
+    final response = await client.get(
+      Uri.parse('$_kServer/enterprises?name=$query'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      }..addAll(_authHeaders),
+    );
+
+    if (response.statusCode == 200) {
+      final List<EnterpriseModel> enterprisesList = List<EnterpriseModel>.empty(growable: true);
+      final Map<String, dynamic> bodyJson = json.decode(response.body);
+      bodyJson['enterprises'].forEach((el) {
+        enterprisesList.add(EnterpriseModel.fromJson(el));
+      });
+
+      return enterprisesList;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<InvestorModel> signIn(String email, String password) async {
     Map<String, dynamic> requestBody = <String, dynamic>{
       'email': email,
       'password': password,
     };
-    http.Response response = await http.post(
-      Uri.parse('$server/api/$_apiVersion/users/auth/sign_in'),
+
+    final response = await client.post(
+      Uri.parse('$_kServer/users/auth/sign_in'),
       body: json.encode(requestBody),
       headers: <String, String>{'Content-Type': 'application/json'},
     );
-    if (response.statusCode == 200) {
-      _authHeaders['uid'] = response.headers['uid']!;
-      _authHeaders['access-token'] = response.headers['access-token']!;
-      _authHeaders['client'] = response.headers['client']!;
-    }
-    return json.decode(response.body);
-  }
 
-  /// Does a search and return a list of searched enterprises.
-  /// Returns an empty list on communication errors or when nothing is found and throws on connection error.
-  static Future<List<Enterprise>> search(String query) async {
-    http.Response response = await http.get(
-      Uri.parse('$server/api/$_apiVersion/enterprises?name=$query'),
-      headers: <String, String>{'Content-Type': 'application/json'}..addAll(_authHeaders),
-    );
-    List<Enterprise> enterprises = List<Enterprise>.empty(growable: true);
     if (response.statusCode == 200) {
-      List<dynamic> enterprisesFromApi = json.decode(response.body)['enterprises'];
-      for (var item in enterprisesFromApi) {
-        enterprises.add(EnterpriseModel.fromJson(item));
+      try {
+        final Map<String, dynamic> bodyJson = json.decode(response.body);
+        InvestorModel investor = InvestorModel.fromJson(bodyJson['investor']);
+        return investor;
+      } catch (_) {
+        throw ServerException();
       }
+    } else {
+      throw ServerException();
     }
-    return enterprises;
   }
 }
---
+++ empresas/test/shared/data/datasources/enterprises_remote_api_test.dart
@@ -0,0 +1,185 @@
+import 'dart:convert';
+
+import 'package:empresas/features/list_enterprises/data/models/enterprise_model.dart';
+import 'package:empresas/features/list_enterprises/domain/entities/enterprise.dart';
+import 'package:empresas/features/sign_in/data/models/investor_model.dart';
+import 'package:empresas/shared/data/datasources/enterprises_remote_api.dart';
+import 'package:empresas/shared/errors/exceptions.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../fixtures/fixture_reader.dart';
+import 'enterprises_remote_api_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+main() {
+  late EnterprisesRemoteApi enterprisesApi;
+  late MockClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockClient();
+    enterprisesApi = EnterprisesRemoteApi(client: mockHttpClient);
+  });
+
+  group('ListEnterprises', () {
+    final String tQuery = 'Superstore';
+    final List<Enterprise> tEnterprisesList = List<Enterprise>.empty(growable: true);
+    final Map<String, dynamic> fixtureJson = json.decode(fixtureReader('enterprises.json'));
+    fixtureJson['enterprises'].forEach((el) {
+      tEnterprisesList.add(EnterpriseModel.fromJson(el));
+    });
+
+    test('Should get a list of ALL enterprises', () {
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response(fixtureReader('enterprises.json'), 200),
+      );
+
+      enterprisesApi.getAllEnterprises();
+
+      verify(mockHttpClient.get(
+        Uri.parse('https://empresas.ioasys.com.br/api/v1/enterprises'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+      ));
+    });
+
+    test('Should return List<Enterprise> with ALL enterprises when the response is HTTP 200',
+        () async {
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response(fixtureReader('enterprises.json'), 200),
+      );
+
+      final result = await enterprisesApi.getAllEnterprises();
+
+      expect(result, tEnterprisesList);
+    });
+
+    test('Should thrown a ServerException on getAllEnterprises when the response is NOT HTTP 200',
+        () async {
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response('Something went wrong', 404),
+      );
+
+      final call = enterprisesApi.getAllEnterprises;
+
+      expect(call, throwsA(TypeMatcher<ServerException>()));
+    });
+
+    test('Should get a list of SEARCHED enterprises', () {
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response(fixtureReader('enterprises.json'), 200),
+      );
+
+      enterprisesApi.searchEnterprises(tQuery);
+
+      verify(mockHttpClient.get(
+        Uri.parse('https://empresas.ioasys.com.br/api/v1/enterprises?name=$tQuery'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+      ));
+    });
+
+    test('Should return List<Enterprise> with SEARCHED enterprises when the response is HTTP 200',
+        () async {
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response(fixtureReader('enterprises.json'), 200),
+      );
+
+      final result = await enterprisesApi.searchEnterprises(tQuery);
+
+      expect(result, tEnterprisesList);
+    });
+
+    test('Should thrown a ServerException on searchEnterprises when the response is NOT HTTP 200',
+        () async {
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response('Something went wrong', 404),
+      );
+
+      final call = enterprisesApi.searchEnterprises;
+
+      expect(call(tQuery), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+
+  group('SignIn', () {
+    final String tEmail = 'testeapple@ioasys.com.br';
+    final String tPassword = '1234';
+    final Map<String, dynamic> tRequestBody = <String, dynamic>{
+      'email': tEmail,
+      'password': tPassword,
+    };
+    final InvestorModel tInvestor = InvestorModel.fromJson(
+      json.decode(fixtureReader('user_success.json'))['investor'],
+    );
+
+    test('Should perform a sign in', () {
+      when(mockHttpClient.post(
+        any,
+        body: anyNamed('body'),
+        headers: anyNamed('headers'),
+      )).thenAnswer(
+        (_) async => http.Response(fixtureReader('user_success.json'), 200),
+      );
+
+      enterprisesApi.signIn(tEmail, tPassword);
+
+      verify(mockHttpClient.post(
+        Uri.parse('https://empresas.ioasys.com.br/api/v1/users/auth/sign_in'),
+        body: json.encode(tRequestBody),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+      ));
+    });
+
+    test('Should return a Investor when the response is HTTP 200 and the sign in was successful',
+        () async {
+      when(mockHttpClient.post(
+        any,
+        body: anyNamed('body'),
+        headers: anyNamed('headers'),
+      )).thenAnswer(
+        (_) async => http.Response(fixtureReader('user_success.json'), 200),
+      );
+
+      final result = await enterprisesApi.signIn(tEmail, tPassword);
+
+      expect(result, tInvestor);
+    });
+
+    test(
+        'Should throw a ServerException when the response is HTTP 200 and the sign in was NOT successful',
+        () async {
+      when(mockHttpClient.post(
+        any,
+        body: anyNamed('body'),
+        headers: anyNamed('headers'),
+      )).thenAnswer(
+        (_) async => http.Response(fixtureReader('user_fail.json'), 200),
+      );
+
+      final call = enterprisesApi.signIn(tEmail, tPassword);
+
+      expect(call, throwsA(TypeMatcher<ServerException>()));
+    });
+
+    test('Should thrown a ServerException on signIn when the response is NOT HTTP 200', () async {
+      when(mockHttpClient.post(
+        any,
+        body: anyNamed('body'),
+        headers: anyNamed('headers'),
+      )).thenAnswer(
+        (_) async => http.Response('Something went wrong', 404),
+      );
+
+      final call = enterprisesApi.signIn(tEmail, tPassword);
+
+      expect(call, throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+}
